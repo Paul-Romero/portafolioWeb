c2a97a5b8fb840aefbdb503bdab9adb87e4d8168
Paul-Romero <paul-andre2000@hotmail.com>
2021-04-11 18:47:32 -0500

    SASS y PHP

---
+++ app.js
@@ -0,0 +1,18 @@
+//Precargador de la página
+window.onload = function(){
+    $('#onldr').fadeOut();
+    $('body').removeClass('hidden');
+}
+//Ventana emergente email
+var btnOpenPopUp = document.getElementById("btn-open-popup"),
+    overlay = document.getElementById("overlay"),
+    popUp = document.getElementById("popup"),
+    btnClosePopUp = document.getElementById("btn-close-popup");
+btnOpenPopUp.addEventListener('click', function() {
+    overlay.classList.add('active');
+    popUp.classList.add('active');
+});
+btnClosePopUp.addEventListener('click', function() {
+    overlay.classList.remove('active');
+    popUp.classList.remove('active');
+});
---
+++ send-mail.php
@@ -0,0 +1,20 @@
+<?php
+    $origin = $_POST["from"];
+    $target = $_POST["to"];
+    $subject = $_POST["subject"];
+
+    echo "De: ".$origin." Para: ".$target;
+
+    /* $header = "From: ".$origin."\r\n";
+    $header .= "X-Mailer: PHP/".phpversion()."\r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $message = "Enviado por: ".$origin."\r\n";
+    $message .= "Mensaje: ".$subject."\r\n";
+    $message .= "Fecha: ".date('d/m/y',time());
+    $topic = "Portafolio Mail-box";
+
+    mail($target,$topic,utf8_decode($message),$header);
+    header("Location:index.html"); */
+?>
